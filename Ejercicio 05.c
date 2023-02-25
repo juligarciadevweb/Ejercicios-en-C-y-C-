f2b2ca12c63104603366868fb0827a9d5ca9016f
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-02-25 20:39:15 -0300

    Mostrar en consola

---
+++ Ejercicio 05.c
@@ -0,0 +1,22 @@
+/*
+
+Escribir un programa en C en el que se ingrese por teclado 
+el valor de una variable entera, otra flotante y 
+un carácter y se muestre su valor por el monitor:
+
+*/
+
+int main(){
+    int a;
+    float b;
+    char c;
+
+    scanf("%d", a);
+    scanf("%f", b);
+    scanf("%c", c);
+
+    printf("El valor de a es: %d", a);
+    printf("El valor de b es: %f", b);
+    printf("El valor de c es: %c", c);
+}
+
