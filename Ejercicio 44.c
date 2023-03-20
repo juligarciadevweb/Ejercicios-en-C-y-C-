43bb5f400137e6428e617d18100549a7f17a0ba6
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-03-20 10:19:54 -0300

    Practica del ciclo while
    Imprimir los números del -50 al 0

---
+++ Ejercicio 44.c
@@ -0,0 +1,13 @@
+//3 - Imprimir los números del -50 al 0.
+int main(){
+    int x;
+    int x = -50;
+    while (x <= 0)
+    {
+        printf("%d", x);
+        printf(" _ ");
+        x = x + 1;
+    }
+    return 0;
+
+}
