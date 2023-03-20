6078b9d116ee171ffbf8046d784715e98bb5b262
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-03-20 10:17:14 -0300

    Practica del ciclo while
    Imprimir los números del 50 al 100

---
+++ Ejercicio 43.c
@@ -0,0 +1,14 @@
+//2 - Imprimir los números del 50 al 100.
+
+int main(){
+    int x;
+    int x = 50;
+    while (x <= 100)
+    {
+        printf("%d", x);
+        printf(" _ ");
+        x = x + 1;
+    }
+    return 0;
+
+}
