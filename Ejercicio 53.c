7af2c858be0f2a6a8fbc05a9fb0daa1ae828e7b3
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2023-03-23 09:29:39 -0300

    Uso de constante simple
    En el aula que cursa AED la suma de números de mujeres con el de varones es 80 y su diferencia es 10. 
    Se desea saber cuántos hombres y mujeres tiene el curso

---
+++ Ejercicio 53.c
@@ -0,0 +1,14 @@
+/*
+
+En el aula que cursa AED la suma de números de mujeres con el de varones es 80 y su diferencia es 10. 
+Se desea saber cuántos hombres y mujeres tiene el curso
+
+*/
+
+#define totalAlumnos 80
+int main(){
+    int varones = 50; //Suponiendo que la mitad seria + 10
+    int mujeres = 80 - 50;
+    printf("La cantidad de mujeres seria %d", mujeres);
+    return 0;
+}
